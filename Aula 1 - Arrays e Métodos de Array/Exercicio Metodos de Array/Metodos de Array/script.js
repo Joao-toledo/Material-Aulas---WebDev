760dec4ff93b4a9fbfbb20c631738115e377b078
joao toledo <jvmstoledo@gmail.com>
2025-08-07 22:41:04 -0300

    aula

--- Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
+++ Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
@@ -1,46 +1,50 @@
 function executarPush() {
   const array = ['🍎', '🍌'];
+  array.push("🍇"); //adiciona o elemento no final
   const resultado = array;
   document.getElementById('resultado-push').textContent = resultado;
 }
 
 function executarPop() {
   const array = ['🍎', '🍌', '🍇'];
+  array.pop(); //remove o ultimo elemento
   const resultado = array;
   document.getElementById('resultado-pop').textContent = resultado;
 }
 
 function executarShift() {
   const array = ['🍎', '🍌', '🍇'];
+  array.shift(); //remove o primeiro elemento
   const resultado = array;
   document.getElementById('resultado-shift').textContent = resultado;
 }
 
 function executarUnshift() {
   const array = ['🍌', '🍇'];
-  const resultado = 'Resultado da operação unshift';
+  array.unshift("🍓"); //adiciona o elemento no começo
+  const resultado = array;
   document.getElementById('resultado-unshift').textContent = resultado;
 }
 
 function executarIncludes() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = array;
+  const resultado = array.includes("🥝"); //confere se tem o elemento 
   document.getElementById('resultado-includes').textContent = resultado;
 }
 
 function executarReverse() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = array;
+  const resultado = array.reverse(); //inverte os elementos
   document.getElementById('resultado-reverse').textContent = resultado;
 }
 
 function executarSort() {
-  const array = [3, 1, 4, 2];
+  const array = [3, 1, 4, 2, 11];
   const array2 = ["Brasil", "Argentina", "Noruega", "Italia"];
 
-  const resultado = array;
+  const resultado = array.sort((a, b) => b - a); //serve para ver se o número é maior ou menor. 11 > 2 por exemplo
   document.getElementById('resultado-sort').textContent = resultado;
-  const resultado2 = array2;
+  const resultado2 = array2.sort().reverse(); //sort = ordem alfabetica
   document.getElementById('resultado-sort2').textContent = resultado2;
 }
 
@@ -73,31 +77,34 @@ function executarJoin() {
 
 function executarForEach() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação forEach';
+  const resultado = array.forEach(pegaItem => console.log(pegaItem));
   document.getElementById('resultado-foreach').textContent = resultado;
 }
 
 function executarMap() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação map';
+  const resultado = array.map(pegaItem => `✅ ${pegaItem}`)
   document.getElementById('resultado-map').textContent = resultado;
 }
 
  function executarMapHTML() {
     const dispositivos = ['🎮', '🕹️', '💻'];
-    const resultado = 'Resultado da operação map inserindo HTML';
+    const resultado = dispositivos.map((pegaItem, index) => {
+      return `<h1> Item ${index + 1} promoção!</h1>
+      <p> ${pegaItem}</p>`
+    })
     document.getElementById('resultado-map-html').innerHTML = resultado.join('<br>');
   }
 
 function executarFilter() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação filter';
+  const resultado = array.filter(pegaItem => pegaItem === "🍎");
   document.getElementById('resultado-filter').textContent = resultado;
 }
 
 function executarFind() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação find';
+  const resultado = array.find(pegaItem => pegaItem === "🍇");
   document.getElementById('resultado-find').textContent = resultado;
 }
 
@@ -109,7 +116,9 @@ function executarFindIndex() {
 
 function executarReduce() {
   const array = [1, 2, 3, 4];
-  const resultado = 'Resultado da operação reduce';
+  const resultado = array.reduce((totalAcumulado, valorAtual)=> {
+    return totalAcumulado + valorAtual
+  });
   document.getElementById('resultado-reduce').textContent = resultado;
 }
 
@@ -134,3 +143,20 @@ function executarEncadeamento() {
 }
 
 
+
+// // Declarar uma array
+// let nomes = ["Joao", 1, "Fulano"];
+// console.log(nome[2])
+// //Atualizar o valor de um item do array
+// nomes[2] = "Matheus Bidu"
+// //Como saber o tamanho de um array
+// nomes.length
+
+
+// const ordenar = (a, b) => a - b
+
+// const ordenar = a => a - 2
+
+// function ordenar (a){
+//   return a - 2
+// }
