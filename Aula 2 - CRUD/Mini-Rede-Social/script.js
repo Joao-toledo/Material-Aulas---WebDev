8e7c302202917efe3ae2f4021f921d1251585835
joao toledo <jvmstoledo@gmail.com>
2025-08-14 22:31:26 -0300

    ADA

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,74 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+    mostrarPost();
+
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+
+//CREATE
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault();
+    
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: new Date().toLocaleString()
+    }
+
+    posts.unshift(novoPost)
+    mostrarPost()
+
+}
+
+//READ
+function mostrarPost(){
+    //Pegando elemento onde os tweets serão inseridos
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ``
+
+    //Passando pelo array criando um tweet para cada um
+    posts.forEach(pegaItem => {
+
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            <img src="${pegaItem.image}"/>
+            <h3>Categoria: ${pegaItem.category}</h3>
+            <h5>Data e hora: ${pegaItem.date}</h5>
+            <button>Editar</button> 
+            <button>Apagar</button>
+        `
+        listaPosts.append(cardPost)
+    })
+}
+
+//UPDATE
+function aditarPost(){}
+
+//DELETE
+function deletarPost(){}
+
+
+
+
+//Exmeplo
+// const pessoa = {
+//     nome: "João",
+//     idade: 18,
+//     "Empresas trabalhadas": [],
+//     isAdmin: true
+// }
+
+// console.log(pessoa.nome)
+// console.log(pessoa[idade])
